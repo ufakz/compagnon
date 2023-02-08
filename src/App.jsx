021acda660f199de955ad27e5f6fcb1f0fd7cb68
Farouq Umar <ufakz99@gmail.com>
2023-02-08 13:54:55 +0100

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
 import { useState } from "react";
+import Home from "./components/Home";
 import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
-  const [prompt, setPrompt] = useState("");
-  const [imgLink, setImgLink] = useState("");
-
-  const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  });
-
-  const openai = new OpenAIApi(configuration);
-  const generateImage = async () => {
-    const res = await openai.createImage({
-      prompt: prompt,
-      n: 1,
-      size: "1024x1024",
-    });
-
-    setImgLink(res.data.data[0].url);
-  };
-
   return (
     <div className="app-main">
-      <h3>Generate an image</h3>
-      <input
-        className="app-input"
-        placeholder="Enter what you want to generate"
-        onChange={(e) => setPrompt(e.target.value)}
-      />
-      <button onClick={generateImage}>Generate an image </button>
-      {imgLink.length > 0 ? <img className="result-img" src={imgLink || ""} alt="image" />: <></>}
+      <Home />
     </div>
   );
 }

41682781e538bce2ff977e12b6fa82585a3861cf
Farouq Umar <ufakz99@gmail.com>
2023-02-21 00:04:06 +0100

    fix: add scroll to textarea

--- src/options.js
+++ src/options.js
@@ -25,11 +25,10 @@ export const options = [
     options: {
       model: "text-davinci-003",
       temperature: 0.9,
-      max_tokens: 150,
+      max_tokens: 2048,
       top_p: 1,
       frequency_penalty: 0.0,
       presence_penalty: 0.6,
-      stop: [" Human:", " AI:"],
     },
   },
 ];
